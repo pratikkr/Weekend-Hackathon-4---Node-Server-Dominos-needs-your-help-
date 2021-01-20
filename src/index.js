b1bba7aca51393ccf9220a173200a33739a05556
pratik kumar <kumarpratik861@gmail.com>
2021-01-21 01:03:55 +0530

    changed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome to Dominos!");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.statusMessage = "Not found";
+    res.end();
+  }
 }
+
+httpServer.listen(8081);
 
 module.exports = httpServer;
